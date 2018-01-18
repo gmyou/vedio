b9d5fe2681182179b225810e5b2956dbb819a4e4
유근명 <gmyou@onnuridmc.com>
2018-01-18 15:56:59 +0900

    Test Banner 삽입

--- src/sample.php
+++ src/sample.php
@@ -12,6 +12,44 @@
     <title>Sample javascript VPAID ads</title>
   </head>
   <body>
+
+  <div id="banner">
+    <script type='text/javascript'>
+      !function (w,d,s,u,t,ss,fs) {
+          if(w.exelbidtag)return;t=w.exelbidtag={};if(!window.t) window.t = t;
+          t.push = function() {t.callFunc?t.callFunc.apply(t,arguments) : t.cmd.push(arguments);};
+          t.cmd=[];ss = document.createElement(s);ss.async=!0;ss.src=u;
+          fs=d.getElementsByTagName(s)[0];fs.parentNode.insertBefore(ss,fs);
+      }(window,document,'script','//st2.exelbid.com/js/ads.js');
+    </script>
+
+    <script type='text/javascript'>
+        // You can get add request result
+        function ExelbidResponseCallback_187afd1e4280b2678be3dbd23570278b5ebb132f(result){
+            if(result.status == 'OK'){
+                console.log('OK');
+            }else if(result.status == 'NOBID'){
+                console.log('NOBID');
+            }else if(result.status == 'ERROR'){
+                console.log('ERROR');
+            }
+        };
+        exelbidtag.push(function () {
+            exelbidtag.initAdBanner('187afd1e4280b2678be3dbd23570278b5ebb132f', 320, 50, 'div-exelbid-187afd1e4280b2678be3dbd23570278b5ebb132f')
+                .setResponseCallback(ExelbidResponseCallback_187afd1e4280b2678be3dbd23570278b5ebb132f)
+                .setTestMode(true);
+        });
+    </script>
+    <!-- Ad Space -->
+    <div id='div-exelbid-187afd1e4280b2678be3dbd23570278b5ebb132f'>
+        <script type='text/javascript'>
+            exelbidtag.push(function () {
+                exelbidtag.loadAd('187afd1e4280b2678be3dbd23570278b5ebb132f');
+            });
+        </script>
+    </div>
+  </div>
+
   <div id="container">
     <header>Sample javascript VPAID ads</header>
 
